bf9de5c8f743ebbb948fbd3e872411743a134ab2
Sanidhya27 <cool.sanidhyagwl@gmail.com>
2017-09-21 19:46:19 +0530

    second

--- services/views.py
+++ services/views.py
@@ -9,10 +9,10 @@ def display(request):
 		obj=ProductRequest.objects.create(quantityname=p,Department=request.POST['Demandedby'],quantitydemanded=request.POST['QuantityDemanded'],quantitysupplied=request.POST['QuantitySupplied'])
 		obj.save()
 		form=ServiceRequest(request.POST)
-		return render(request,'Product/index.html',{'form':form})
+		return render(request,'Product/ok.html',{'form':form})
 	if request.method=='GET':
 		form=ServiceRequest(request.POST)
-		return render(request,'Product/index.html',{'form':form})
+		return render(request,'Product/ok.html',{'form':form})
 def index(request):
 	if request.method=='GET':
 		form=ServiceRequest(request.GET)
