9d0818e11967df7ded8658c7c3f21463419879a3
Sanidhya27 <cool.sanidhyagwl@gmail.com>
2017-09-25 01:34:50 +0530

    filter

--- services/admin.py
+++ services/admin.py
@@ -5,6 +5,7 @@ from services.models import Product,ProductRequest
 class ProductAdmin(admin.ModelAdmin):
 	list_display=['name']
 class ProductRequestAdmin(admin.ModelAdmin):
+	list_filter=['Department']
 	list_display=['Department','quantityname','quantitydemanded','quantitysupplied']
 admin.site.register(Product,ProductAdmin)
 admin.site.register(ProductRequest,ProductRequestAdmin)
